487775f205ac17fe297efc11ac6bb8885bad03bc
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-08 12:46:21 -0700

    coded PingRequest class

--- request_classes.py
+++ request_classes.py
@@ -2,8 +2,9 @@
 #
 #
 
-
-from typing import Tuple, Any 
+import socket
+import time
+from typing import Tuple, Any
 
 class RequestService():
     #the abstract class defining the behavior of concrete request classes
@@ -14,4 +15,16 @@ class RequestService():
 class PingRequest(RequestService):
     #creates an ICMP packet and pings the given server
     def runRequest(host: str, ttl: int, timeout: int, sequence_number: int) -> Tuple[Any, float] | Tuple[Any, None]:
-        pass
+        with socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP) as sock:
+            sock.setsockopt(socket.IPPROTO_IP, socket.IP_TTL, ttl)
+            sock.settimeout(timeout)
+            packet: bytes = ""
+            sock.sendto(packet, (host, 1))
+            start: float = time.time()
+            try:
+                data, addr = sock.recvfrom(1024)
+                end: float = time.time()
+                total_time = (end-start)*1000
+                return addr, total_time
+            except socket.timeout:
+                return None, None
