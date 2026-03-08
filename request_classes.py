907074a0151e9048c1db1eb3ac192072a1d45ec8
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-08 12:29:12 -0700

    started PingRequest class

--- request_classes.py
+++ request_classes.py
@@ -3,9 +3,15 @@
 #
 
 
+from typing import Tuple, Any 
+
 class RequestService():
     #the abstract class defining the behavior of concrete request classes
     def runRequest():
         pass
 
 
+class PingRequest(RequestService):
+    #creates an ICMP packet and pings the given server
+    def runRequest(host: str, ttl: int, timeout: int, sequence_number: int) -> Tuple[Any, float] | Tuple[Any, None]:
+        pass
