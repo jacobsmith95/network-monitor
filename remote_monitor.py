f382e6f693aa1d1c088aa33c5d86e5860f68bf26
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-11 09:33:37 -0700

    started RemoteClient with ReadSettings and WriteSettings methods

--- remote_monitor.py
+++ remote_monitor.py
@@ -2,15 +2,50 @@
 #
 #
 
-
+import json
 from abc import ABC, abstractmethod
 
 def main():
     pass
 
 
-class Client():
-    pass
+class AbstractClient(ABC):
+    """ """
+    @abstractmethod
+    def ReadSettings():
+        pass
+
+    @abstractmethod
+    def WriteSettings():
+        pass
+
+    @abstractmethod
+    def CommsHandler():
+        pass
+
+    @abstractmethod
+    def ServicesHandler():
+        pass
+
+    @abstractmethod
+    def ExitProgram():
+        pass
+
+
+class RemoteClient(AbstractClient):
+    """ """
+    def ReadSettings(monitor_id: str) -> str:
+        with open(f"monitor_config_{monitor_id}.txt", "r") as file:
+            config = json.load(file)
+        file.close()
+        return config
+    
+    def WriteSettings(monitor_id: str, config: str) -> None:
+        with open(f"monitor_config_{monitor_id}.txt", "w") as file:
+            json.dump(config, file)
+        file.close()
+        return
+
 
 if __name__ == "__main__":
     main()
