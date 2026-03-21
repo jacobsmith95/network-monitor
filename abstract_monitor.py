fb45c9363f31c8bc9d018547cf38733ec252ba22
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-21 15:42:40 -0700

    moved AbstractClient to abstract_monitor

--- abstract_monitor.py
+++ abstract_monitor.py
@@ -6,3 +6,32 @@
 from abc import ABC, abstractmethod
 
 
+class AbstractClient(ABC):
+    """ """
+    @abstractmethod
+    def RunMonitor():
+        pass
+
+    @abstractmethod
+    def ReadSettings():
+        pass
+
+    @abstractmethod
+    def WriteSettings():
+        pass
+
+    @abstractmethod
+    def SetSocketHandler():
+        pass
+
+    @abstractmethod
+    def SetServiceHandler():
+        pass
+
+    @abstractmethod
+    def SetCommsHandler():
+        pass
+
+    @abstractmethod
+    def ExitProgram():
+        pass
--- remote_monitor.py
+++ remote_monitor.py
@@ -6,6 +6,7 @@ import json
 import sys
 import socket
 import threading
+from abstract_monitor import AbstractClient
 from abc import ABC, abstractmethod
 from rich.console import Console
 from typing import Tuple
@@ -18,37 +19,6 @@ def main():
     sockethandler = SocketHandler()
     client.SetSocketHandler(sockethandler)
     client.RunMonitor()
-
-
-class AbstractClient(ABC):
-    """ """
-    @abstractmethod
-    def RunMonitor():
-        pass
-
-    @abstractmethod
-    def ReadSettings():
-        pass
-
-    @abstractmethod
-    def WriteSettings():
-        pass
-
-    @abstractmethod
-    def SetSocketHandler():
-        pass
-
-    @abstractmethod
-    def SetServiceHandler():
-        pass
-
-    @abstractmethod
-    def SetCommsHandler():
-        pass
-
-    @abstractmethod
-    def ExitProgram():
-        pass
 
 
 class AbstractHandler(ABC):
