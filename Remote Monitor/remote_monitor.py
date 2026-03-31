51b53d466e0cac0b18b38b748c19acf21f31171a
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-31 10:11:08 -0700

    added main function to remote_monitor

--- Remote Monitor/remote_monitor.py
+++ Remote Monitor/remote_monitor.py
@@ -5,3 +5,9 @@
 from monitor_classes import RemoteClient, SocketHandler, CommsHandler, ServiceHandler
 
 
+def main():
+    #console = Console()
+    client = RemoteClient()
+    sockethandler = SocketHandler()
+    client.SetSocketHandler(sockethandler)
+    client.RunMonitor()
