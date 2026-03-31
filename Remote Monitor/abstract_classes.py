da35639e33593bea229268cb2d231e41e075e0ef
Jacob O. Smith <jacobsmith95@gmail.com>
2026-03-31 10:07:10 -0700

    moved abstract_classes to Remote Monitor Folder

---
+++ Remote Monitor/abstract_classes.py
@@ -0,0 +1,57 @@
+#
+#
+#
+
+from abc import ABC, abstractmethod
+
+
+class AbstractRequest(ABC):
+    """the abstract class defining the behavior of concrete request classes"""
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def NetRequest(self):
+        pass
+
+    @abstractmethod
+    def RunRequest(self):
+        pass
+
+
+class AbstractClient(ABC):
+    """ """
+    @abstractmethod
+    def RunMonitor():
+        pass
+
+    @abstractmethod
+    def ReadSettings():
+        pass
+
+    @abstractmethod
+    def WriteSettings():
+        pass
+
+    @abstractmethod
+    def SetSocketHandler():
+        pass
+
+    @abstractmethod
+    def SetServiceHandler():
+        pass
+
+    @abstractmethod
+    def SetCommsHandler():
+        pass
+
+    @abstractmethod
+    def ExitProgram():
+        pass
+
+
+class AbstractHandler(ABC):
+    """ """
+    @abstractmethod
+    def RunHandler():
+        pass
