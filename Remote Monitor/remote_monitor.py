f8edbfd58cddca4615986d56007fd735a832a41c
Jacob O. Smith <jacobsmith95@gmail.com>
2026-04-02 08:26:54 -0700

    added request_classes import statement

--- Remote Monitor/remote_monitor.py
+++ Remote Monitor/remote_monitor.py
@@ -3,18 +3,24 @@
 #
 
 from monitor_classes import RemoteClient, SocketHandler, CommsHandler, ServiceHandler
+from request_classes import PingService, TracerouteService, HttpService, HttpsService, NtpService, DnsService, TcpPortService, UdpPortService
 
 
 def main():
     """Loads the concrete client class, the concrete handler classes, and injects the handlers into the client"""
     #console = Console()
+
     client = RemoteClient()
+
     sockethandler = SocketHandler()
     commshandler = CommsHandler()
     servicehandler = ServiceHandler()
     client.SetSocketHandler(sockethandler)
     client.SetCommsHandler(commshandler)
     client.SetServiceHandler(servicehandler)
+
+    
+
     client.RunMonitor()
 
 if __name__ == "__main__":
