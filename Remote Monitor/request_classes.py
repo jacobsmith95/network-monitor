a64637b48dccf35f055af8ddbc1cb27fb6bdd8c3
Jacob O. Smith <jacobsmith95@gmail.com>
2026-04-12 20:34:27 -0700

    added doc string to RunRequest method in TcpPortService class

--- Remote Monitor/request_classes.py
+++ Remote Monitor/request_classes.py
@@ -226,6 +226,7 @@ class TcpPortService(AbstractRequest):
             return False, f"Failed to check TCP port {port} on {ip_address} due to an error: {exc}."
         
     def RunRequest(self, monitor_id: str, url: str, args: Tuple, out_queue: Queue, end_event: threading.Event):
+        """Deconstructs the arg tuple and runs a NetRequest each given time interval."""
         port, interval = args[0], args[1]
         while not end_event.is_set():
             data = self.NetRequest(url, port)
