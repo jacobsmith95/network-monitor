ba29a6c32ad346849829469ab4dae7c4f03aca4b
Jacob O. Smith <jacobsmith95@gmail.com>
2026-04-15 12:08:27 -0700

    added final doc string to server_manager

--- Server Manager/server_manager.py
+++ Server Manager/server_manager.py
@@ -1,6 +1,6 @@
 #This file contains the main function for the server manager.
 #It injects the appropriate dependencies and runs the server using the in-built class method.
-#
+#This program communicates configuration information to the individual remote monitors and reports the results to the user.
 
 
 def main():
